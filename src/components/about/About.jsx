dd67faa966f425c260f2b4583f0af2d810141698
Vensan <v.drot@student.inha.uz>
2022-05-31 01:38:39 +0500

    forth commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './About.css'
+import ME from '../../img/me-about.jpg'
 
 const About = () => {
   return (
-    <section id='about'></section>
+    <section id='about'>
+       
+      <h5>Get to know</h5>  
+      <h2>About me</h2>
+  
+      <div className='container about__container'>
+       
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt="About image" />
+          </div>
+        </div>
+
+        <div className='about_content'>
+          <div className='about__cards'>
+
+            <article className='about__card'>
+            <i class="fa-solid fa-award about__icon" ></i>
+              <h5>Experience</h5>
+              <small>2+ YearsWorking</small>
+            </article>
+
+            <article className='about__card'>
+            <i class="fa-solid fa-users about__icon" ></i>
+              <h5>Clients</h5>
+              <small>3+ Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+            <i class="fa-solid fa-briefcase about__icon"></i>
+              <h5>Projects</h5>
+              <small>10+ Completed</small>
+            </article>
+          </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur cumque doloremque eos voluptates, iure fuga laborum minima ut, impedit accusamus facilis repellat, ipsam iste dolorem. Ut voluptate dolore aliquid numquam.</p>
+        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,111 @@
 import React from 'react'
 import './Experience.css'
+import {BsPatchCheckFill } from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'></section>
+    <section id='experience'>
+      <h5>What Skill Do I Have</h5>
+      <h2>My Experience</h2>
+      <div class='container experience__container'>
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+           <BsPatchCheckFill class='experience__details-icons' />
+            <div>
+              <h4>HTML</h4>
+            <small class='text-light'>Experienced</small>
+            </div>
+            </article>
+            <article className='experience__details'>
+           <BsPatchCheckFill class='experience__details-icons' />
+            <div>
+              <h4>CSS</h4>
+            <small class='text-light'>Experienced</small>
+            </div>
+            </article>
+            <article className='experience__details'>
+           <BsPatchCheckFill class='experience__details-icons' />
+            <div>
+              <h4>BootStrap</h4>
+            <small class='text-light'>Intermediate</small>
+            </div>
+            </article>
+            <article className='experience__details'>
+            <BsPatchCheckFill class='experience__details-icons' />
+            <div>
+              <h4>ReactJS</h4>
+            <small class='text-light'>Intermediate</small>
+            </div>
+            </article>
+          </div>
+        </div>
+
+        <div className="experience__backend">
+        <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+           <BsPatchCheckFill class='experience__details-icons' />
+            <div>
+              <h4>PHP</h4>
+            <small class='text-light'>Experienced</small>
+            </div>
+            </article>
+            <article className='experience__details'>
+           <BsPatchCheckFill class='experience__details-icons' />
+            <div>
+              <h4>JavaScript</h4>
+            <small class='text-light'>Experienced</small>
+            </div>
+            </article>
+            <article className='experience__details'>
+           <BsPatchCheckFill class='experience__details-icons' />
+            <div>
+              <h4>NodeJS</h4>
+            <small class='text-light'>Intermediate</small>
+            </div>
+            </article>
+            <article className='experience__details'>
+           <BsPatchCheckFill class='experience__details-icons' />
+            <div>
+              <h4>MySQL</h4>
+            <small class='text-light'>Experienced</small>
+            </div>
+            </article>
+            <article className='experience__details'>
+           <BsPatchCheckFill class='experience__details-icons' />
+            <div>
+              <h4>C++</h4>
+            <small class='text-light'>Experienced</small>
+            </div>
+            </article>
+            <article className='experience__details'>
+           <BsPatchCheckFill class='experience__details-icons' />
+            <div>
+              <h4>C#</h4>
+            <small class='text-light'>Intermediate</small>
+            </div>
+            </article>
+            <article className='experience__details'>
+           <BsPatchCheckFill class='experience__details-icons' />
+            <div>
+              <h4>Python</h4>
+            <small class='text-light'>Intermediate</small>
+            </div>
+            </article>
+            <article className='experience__details'>
+           <BsPatchCheckFill class='experience__details-icons' />
+            <div>
+              <h4>Pascal</h4>
+            <small class='text-light'>Experienced</small>
+            </div>
+            </article>
+          </div>
+        </div>
+     
+      </div>
+    </section>
   )
 }
 
