aa4b147d89598917aa04ef6ffef1e38c2b2d7b81
Vensan <v.drot@student.inha.uz>
2022-10-19 21:02:48 +0500

    New projects

--- src/components/port/Port.jsx
+++ src/components/port/Port.jsx
@@ -5,6 +5,9 @@ import IMG2 from '../../img/bk.webp'
 import IMG3 from '../../img/web.png'
 import IMG4 from '../../img/react.png'
 import IMG5 from '../../img/weather.jpg'
+import IMG6 from '../../img/quote.png'
+import IMG7 from '../../img/mobile.png'
+import IMG8 from '../../img/portfolio5.png'
 
 
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -61,6 +64,25 @@ const data = [
         title: "Weather React app ",
         github: 'https://github.com/VensanDrot/Weather-React-App',
         demo:'https://gleeful-croissant-292f2a.netlify.app/' 
+    },
+    {
+        id: 6,
+        image: IMG6,
+        title: "Quote Heaven Vue ",
+        github: 'https://github.com/VensanDrot/Vue_Test_Task',
+        demo:'https://fantastic-sprinkles-87e2e5.netlify.app/' 
+    },
+    {
+        id: 7,
+        image: IMG7,
+        title: "React Native App",
+        github: 'https://github.com/VensanDrot/Test_Mobile_App',
+    },
+    {
+        id: 8,
+        image: IMG8,
+        title: "Employee, Client Base",
+        github: 'https://github.com/VensanDrot/Admin_Panel_Test',
     }
 ]
 
@@ -73,45 +95,13 @@ const Port = () => {
 
       <div className="container portfolio_container">
 
-
-       {/*<Swiper
-        pagination={{
-          type: "progressbar",
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation]}
-        className="mySwiper"
-      >
-           {
-            data.map(({id,image,title,github,demo})=>{
-                return(
-                    <SwiperSlide>
-                    <article className='portfolio_item' key={id}>
-                    <div className="portfolio_item_img">
-                        <img src={image} alt={title} />
-                    </div>
-                    <h3>{title}</h3>
-                    <div className='portfolio_item_cta'>
-                    <a href={github} target='_blank' className='btn'>Github</a>
-                    <a href={demo} target='_blank' className='btn btn-primary'>Live Demo</a>
-                    </div>
-                </article>
-                </SwiperSlide>
-                )
-            })
-        } 
-      
-    
-      </Swiper>
-    */}
-
         {
             data.map(({id,image,title,github,demo})=>{
                 return(
                     
                     <article className='portfolio_item' key={id}>
                     <div className="portfolio_item_img">
-                        <img src={image} alt={title} />
+                        <img className='image_source' src={image} alt={title} />
                     </div>
                     <h3>{title}</h3>
                     <div className='portfolio_item_cta'>
